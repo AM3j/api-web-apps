fc79be5c0d2f456e0a5447d8e4801c9e01c5a289
am3 <am3@example.com>
2024-08-06 05:24:21 +0300

    Visualization added

--- api_web_apps/github_api/views.py
+++ api_web_apps/github_api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from django.views import View
+import plotly.express as px
+from plotly.offline import plot
+import pandas as pd
 
 # Class Based Views 
 class Github_View(View):
@@ -34,7 +37,8 @@ class Github_View(View):
                 'topics' : item['topics']
             } for item in response_dict['items'][:10] ]
 
-            context = {'status_code': status_code, 'items': items}
+            figuer = self.generate_interactive_figure(items, lang)
+            context = {'status_code': status_code, 'items': items, 'fig': figuer}
         elif status_code == 422:
             message = response_dict['errors'][0]['message']
             context ={'status_code2': status_code, 'message': message}
@@ -42,3 +46,45 @@ class Github_View(View):
             context ={'status_code': 'There is an error'}
 
         return context
+
+    def generate_interactive_figure(self, items, lang):
+        df = pd.DataFrame(items)
+        df['repo_url'] = df.apply(lambda row: f"<a href='{row['url']}' target='_blank'>{row['full_name']}</a>", axis=1)
+        # Create the bar chart
+        fig = px.bar(
+            df,
+            x='repo_url',
+            y='stars',
+            title="Repositories by Star Count",
+            labels={'repo_url': 'Repository Name', 'stars': 'Star Count'},
+            text='stars',
+            color_discrete_sequence=['#013220']  # Set the color of the bars
+        )
+
+        # Customize the layout
+        fig.update_layout(
+            title={
+                'text': f"Top 10 Repositories for {lang}",
+                'y': 0.9,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'
+            },
+            xaxis_title="Urls for repostries",
+            yaxis_title="Star Count",
+            margin=dict(l=20, r=20, t=50, b=20),
+            paper_bgcolor="#f8f9fa",
+            plot_bgcolor="white",
+            width= 800,
+            height= 600
+        )
+
+        fig.update_traces(
+        texttemplate='%{text:.2s}', 
+        textposition='outside',
+        hovertemplate='%{x}<br>Stars: %{y}<extra></extra>',
+        customdata=df[['url']].values
+        )
+
+        plot_div = plot(fig, output_type='div', include_plotlyjs='cdn')
+        return plot_div
